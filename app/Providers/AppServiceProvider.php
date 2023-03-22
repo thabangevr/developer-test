42f7c7a17f8d116f305ba708aab84021d864a616
Thabang <thabangmasango1992@gmail.com>
2023-03-22 21:26:02 +0200

    Register user interface and repository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\Interfaces\UserInterface;
+use App\Repositories\UserRepository;
+use App\Repositories\UserRepository\UserRepository as UserRepositoryUserRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(UserInterface::class, UserRepositoryUserRepository::class);
     }
 
     /**
